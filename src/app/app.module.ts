b0be5af1a046f64972ccddc30b5047a320f75b1c
Alexsander <alexsandergutierreez@gmail.com>
2022-05-10 21:34:19 -0300

    aula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProdutoComponent } from './pages/produto/produto.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { AddProdutoComponent } from './pages/add-produto/add-produto.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    ProdutoComponent,
+    CatalogoComponent,
+    AddProdutoComponent
   ],
   imports: [
     BrowserModule,

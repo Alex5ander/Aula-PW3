dafbe4ce000e9e208f1d66d105144c5dadf959b6
Alexsander <alexsandergutierreez@gmail.com>
2022-06-08 22:10:18 -0300

    ajuste

--- src/app/pages/produto/produto.component.ts
+++ src/app/pages/produto/produto.component.ts
@@ -1,4 +1,7 @@
+import { Produto } from './../../interface/produto';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-produto',
@@ -6,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produto.component.css'],
 })
 export class ProdutoComponent implements OnInit {
-  constructor() {}
+  url = 'http://lucasreno.kinghost.net/loja/produto/';
+  urlProdutos = 'http://lucasreno.kinghost.net/loja/produtos';
+  produto: Produto | undefined = undefined;
+  produtos: Produto[] = [];
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.http.get<Produto[]>(this.urlProdutos).subscribe((produtos) => {
+      this.produtos = produtos;
+    });
+
+    this.route.params.subscribe((params) => {
+      this.http.get<Produto[]>(this.url + params['id']).subscribe((produto) => {
+        this.produto = produto[0];
+      });
+    });
+  }
 }
